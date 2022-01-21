1a1b06d6ffb4fa7b19107912055271d7dc812abe
StefWebDesigner <67237726+StefWebDesigner@users.noreply.github.com>
2022-01-20 20:08:04 -0500

    intial commit

--- src/main/java/CustomArrayList.java
+++ src/main/java/CustomArrayList.java
@@ -1,41 +1,45 @@
 import java.sql.SQLOutput;
 
-/**
- * A fairly simple arraylist implementation extending custom list interface.
- * Default size is 2, grows by size * 2 when needed.
- * When an element is added or removed at an index other elements are not re-arranged.
- *
- * //@param <e>
- */
+//
+//A fairly simple arraylist implementation extending custom list interface.
+//Default size is 2, grows by size * 2 when needed.
+//When an element is added or removed at an index other elements are not re-arranged.
+//
+//@param <e>
+
 public class CustomArrayList<E> implements CustomListInterface<E> {
     private Object[] array;
     private int size;
     private int maxSize;
+    private int lastArrayObject;
+    private int n;
 
-    /**
-     * Default constructor, creates an empty underlying array with maxSize 2
-     */
+//
+//Default constructor, creates an empty underlying array with maxSize 2
+//
     public CustomArrayList() {
         maxSize = 2;
         size = 0;
         array = new Object[maxSize];
     }
 
-    /**
-     * Sized constructor, creates an empty object with maxSize size
-     * @param size the initial size of the underlying array
-     */
-    public CustomArrayList(int size) {
+    //don't have bolucks idea how that is different from the other instruction
+
+//
+//Sized constructor, creates an empty object with maxSize size
+//@param size the initial size of the underlying array
+//
+    public CustomArrayList(int size) { //$$$$$$$$ IS THIS SUPPOSE TO BE THE LENGTH IMITATOR
         maxSize = size;
         size = 0;
         array = new Object[size];
     }
 
-    /**
-     * Element list constructor, takes in variable number of objects and creates an underlying
-     * array large enough to fit them.
-     * @param e
-     */
+//
+//Element list constructor, takes in variable number of objects and creates an underlying
+//array large enough to fit them.
+//@param e
+//
     public CustomArrayList(E ...e) {
         maxSize = size = e.length;
         array = new Object[size];
@@ -46,114 +50,164 @@ public class CustomArrayList<E> implements CustomListInterface<E> {
     }
 
 
-    /**
-     * Adds an object to the underlying array after all previously added objects.
-     * If array needs to grow, it invokes grow method.
-     * @param o object to be added
-     */
+//
+//Adds an object to the underlying array after all previously added objects.
+//If array needs to grow, it invokes grow method.
+//@param o object to be added
+//
     @Override
     public void add(Object o) {
 
-        int n;
-       int selectedN = add();
-
-        //Implement this method
-        // NOTE: if size >= maxSize we need to grow array
         //GROW CHECK
-        if (size == maxSize) {
+        if (size <= maxSize) {
             System.out.println("No need to make the array grow");
-        } else {
+        } else if (size >= maxSize) {
             growArray();
             System.out.println("growArray has been invoked because not enough space");
         }
 
-        //GET THE POSITION OF THE LAST OBJECT IN THE ARRAY
-            //set N with the add(function)
-            n = selectedN;
+        int totalArrayLength = array.length; //DETERMINES THE TOTAL DISTANCE OF THE ARRAY
 
-            //there is a rotation of the array objects
-            for (int i = 0; i < n; i++) {
-                int k, lastArrayObject;
-            }
+        o = array[totalArrayLength + 1]; //THIS WILL TAKE THE MOST BACKEND OF THE ARRAY
 
-        int lastArrayObject = array[array.length-1];
-            for (int k = array.length - 1; k > 0; k--) {
-                array[k] = array[k - 1];
-            }
-
-            array[0] = lastArrayObject;
-
-            //DISPLAY THE RESULTS
-        System.out.println();
-
+        //the other way of going it
+//            //set N with the add(function)
+//            //there is a rotation of the array objects
+//            for (int i = 0; i < size; i++) {
+//                int k, lastArrayObject;
+//            }
+//
+//        lastArrayObject = array[array.length-1];
+//            for (int k = array.length - 1; k > 0; k--) {
+//                array[k] = array[k - 1];
+//            }
+//
+//            array[0] = lastArrayObject;
+//
     }
-    /**
-     * Adds object at specified index, advancing the size of the underlying array. This will
-     * require us to shift all later elements further down the index order
-     * @param index index location where object will be inserted
-     * @param e element to be inserted
-     * @throws IndexOutOfBoundsException
-     */
+//
+//Adds object at specified index, advancing the size of the underlying array. This will
+//require us to shift all later elements further down the index order
+//@param index index location where object will be inserted
+//@param e element to be inserted
+//@throws IndexOutOfBoundsException
+//
     @Override
     public void add(E e, int index) throws IndexOutOfBoundsException {
-        //Implement this method
-    }
 
-    /**
-     * gets the object located at supplied index
-     * @param index index of object to get
-     * @return object located at index
-     * @throws IndexOutOfBoundsException
-     */
+        if (size <= maxSize) {
+            System.out.println("No need to make the array grow");
+        } else if (size >= maxSize) {
+            growArray();
+            System.out.println("growArray has been invoked because not enough space");
+        }
+
+
+
+
+//        for (int i = 0; i < array.length; i++) {
+//            if(array[index] == array[i]) {
+//
+//                //STORE IN TEMP AND SINGLE IT OUT
+//                int temp = (int) array[index];
+//
+//                //CREATE A NEW ARRAY WITHOUT IT
+//                int array[] = new int[size - 1];
+//
+//            } else {
+//                array = null;
+//            }
+//            //I'm sure I have to return something
+//        }
+//
+//    }
+
+//
+//gets the object located at supplied index
+//@param index index of object to get
+//@return object located at index
+//@throws IndexOutOfBoundsException
+//
+
+
     @Override
-    public E get(int index) throws IndexOutOfBoundsException{
-        //Implement this method
+    public E get(int index) throws IndexOutOfBoundsException {
+
+        Object o = array[index];
+        return (E) o; //casting >>> memorize this its like parsing but with generic with
     }
 
-    /**
-     * Emptys the underlying array by setting it's private reference to null and allowing
-     * the old array to be garbage collected.
-     */
+//
+//Emptys the underlying array by setting it's private reference to null and allowing
+//the old array to be garbage collected.
+//
     @Override
     public void clear() {
         //Implement this method
     }
 
-    /**
-     * Check if object o is found within underlying array, using Object.equals() method
-     * @param o object to search for
-     * @return index location of first instance of matching object. -1 if not found.
-     */
+//
+//Check if object o is found within underlying array, using Object.equals() method
+//@param o object to search for
+//@return index location of first instance of matching object. -1 if not found.
+//
     @Override
-    public int contains(Object o) {
+    public int contains(Object o) { //will need to use a forloop with matches for this
         //Implement this method
+        return 0;//leave something like this as a placeholder, and fix it later
     }
 
-    /**
-     * Removes object at specified index from underlying array, we will then
-     * need to shift the remaining elements up in the index order to fill in the gap
-     * @param index index of object to remove from array
-     */
+
+//Removes object at specified index from underlying array, we will then
+//need to shift the remaining elements up in the index order to fill in the gap
+//@param index index of object to remove from array
+
     @Override
     public void remove(int index) {
-        //Implement this method
+
+        //GROW CHECK
+        if (size <= maxSize) {
+            System.out.println("No need to make the array grow");
+        } else if (size >= maxSize) {
+            growArray();
+            System.out.println("growArray has been invoked because not enough space");
+        }
+
+        //GET THE INDEX OF THE ITEM IN THE ARRAY TO BE REMOVED
+        //REMEMEBR TO CAST IT
+
+        for (int i = 0; i < array.length; i++) {
+            if(array[index] == array[i]) {
+
+                //STORE IN TEMP AND SINGLE IT OUT
+                int temp = (int) array[index];
+
+                //CREATE A NEW ARRAY WITHOUT IT
+                int array[] = new int[size - 1];
+
+            } else {
+                array = null;
+            }
+        //I'm sure I have to return something
+        }
+
+        //Extra NOTES:
+        //arr = removeTheElement(arr, index);
     }
 
-    /**
-     * returns size of array. This is the one greater than the index of the most advanced stored object,
-     * not the maxSize which controls growth of the underlying array.
-     * @return one greater than index of most advanced stored object
-     */
+//returns size of array. This is the one greater than the index of the most advanced stored object,
+//not the maxSize which controls growth of the underlying array.
+//@return one greater than index of most advanced stored object
+
     @Override
     public int size() {
-        //Implement this method
+
+        return size;
     }
 
 
-    /**
-     * Doubles the size of the underlying array by creating a new array and copying the
-     * contents of the previous array into it.
-     */
+//Doubles the size of the underlying array by creating a new array and copying the
+//contents of the previous array into it.
     private void growArray(){
         //System.out.println("Growing Array from " + maxSize + " to " + maxSize * 2);
         //set up new array
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,6 +2,15 @@ public class MainClass {
 
     public static void main(String[] args) {
 
+        //to test the list:
+        //instantiate a new list
+        //add values
+        //print the whole list in a loop to see if all values are there in the correct order
+        //then start removing a few
+        //then check again
+        //and continue like this until you are confident you have implemented and tested all
+        //necessary methods or you've covered all your use cases for using the list.
+
 
     }
 
